b5da9a14a1a45810698555ab43060560b8ceca94
Kre4 <denhol2002@gmail.com>
2023-10-21 23:27:55 +0300

    initial commit

---
+++ ibs-application/src/main/java/ru/ibs/app/IbsApplication.java
@@ -0,0 +1,17 @@
+package ru.ibs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+@ComponentScan(basePackages = "ru.ibs")
+public class IbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbsApplication.class, args);
+    }
+
+}
